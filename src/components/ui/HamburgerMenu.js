4e7a5c13903e5047d71c1d56a02786acd50439fa
ericnoonan00 <70498751+ericnoonan00@users.noreply.github.com>
2021-10-02 22:21:42 -0400

    removed unnecessary props call

--- src/components/ui/HamburgerMenu.js
+++ src/components/ui/HamburgerMenu.js
@@ -2,7 +2,7 @@ import classes from "./HamburgerMenu.module.css";
 
 const HamburgerMenu = (props) => {
 	return (
-		<div className={classes.hamburgerMenu} onClick={props.onBurger}>
+		<div className={classes.hamburgerMenu}>
 			<div className={classes.hamBar1}></div>
 			<div className={classes.hamBar2}></div>
 			<div className={classes.hamBar3}></div>
